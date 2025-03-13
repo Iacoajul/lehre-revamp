de370b451ad556182cd8910f898542ccc95dedd0
Nachikyuu <74154496+Nachikyuu@users.noreply.github.com>
2025-03-13 21:55:12 +0100

    Header and Footer, finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <div className='footer-div'>2018-2025 Institut für Digital Humanities, Universität zu Köln | Impressum</div>
+    <div className='footer-div'> <p>2018-2025 Institut für Digital Humanities, Universität zu Köln | </p> <p style={{ cursor: 'pointer', marginLeft: '5px'}}> Impressum </p></div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import './Header.css'
+import IDHLogo from '../assets/logo/IDH_Logo_white_Schrift.svg'
+import IconHat from '../assets/icons/iconhat.svg'
+import IconUser from '../assets/icons/iconuser.svg'
+import IconHelp from '../assets/icons/iconhelp.svg'
+import { Link } from 'react-router-dom' //Navigation für das Logo wird importiert
+
 
 function Header() {
   return (
-    <div className='header-div'>
-       [IDH Logo] 
-      <p>Institut für Digital Humanities <br></br>
-      Universität zu Köln </p>
-
-      <p>Icons rechts: 
-        Studihut, User, Help</p>
+    //Logo anklickbar und führt immer zur Main Page
+    <div className='header-div'> 
+      <Link to="/main">
+        <img className='idh-logo' src={IDHLogo} alt="IDH Logo white Schrift" />
+      </Link>
+      <p>
+        <img className='header-icon' src={IconHat} alt="Hat Icon"/>
+        <img className='header-icon' src={IconUser} alt="User Icon"/>
+        <img className='header-icon' src={IconHelp} alt="Help Icon"/>
+      </p>
     </div>
   )
 }
